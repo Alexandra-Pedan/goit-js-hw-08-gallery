fc473f4eb9907e130f6db3bc000c4ee3ac775a8d
Alexandra-Pedan <sashka28465@gmail.com>
2020-10-10 20:31:28 +0300

    modal

--- gallery-hw.js
+++ gallery-hw.js
@@ -1,5 +1,5 @@
 import gallery from "./gallery-items.js";
-console.log(gallery);
+// console.log(gallery);
 
 const galleryEl = document.querySelector(".js-gallery");
 const cardsMarkup = galleryImg(gallery);
@@ -26,3 +26,32 @@ function galleryImg(gallery) {
     )
     .join("");
 }
+
+galleryEl.addEventListener("click", onOpenModal);
+const lightBox = document.querySelector(".js-lightbox");
+const bigPhoto = document.querySelector(".lightbox__image");
+
+function onOpenModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  lightBox.classList.add("is-open");
+  const linkBigPhoto = event.target.dataset.source;
+  bigPhoto.src = linkBigPhoto;
+  bigPhoto.alt = event.target.alt;
+}
+
+
+
+const closeModalBtn = document.querySelector('[data-action="close-lightbox"]')
+closeModalBtn.addEventListener("click", onCloseModal);
+
+function onCloseModal() {
+  lightBox.classList.remove("is-open");
+  bigPhoto.removeAttribute('src');
+  bigPhoto.removeAttribute("alt");
+}
+
+
+
